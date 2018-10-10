0f4abd13b25dfc79c5e7422006cd38d7e1ecf12e
yuriikhomych <yuriykhomich@gmail.com>
2018-10-10 21:40:41 +0300

    Create urls for ArticleListView, FeedListView and FeedCreateView

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import FeedCreateView, FeedListView, ArticleListView
 
 app_name = 'news'
 urlpatterns = [
+    path('feeds/create', FeedCreateView.as_view(), name='feeds-create'),
+    path('feeds/', FeedListView.as_view(), name='feeds-list'),
+    path('articles/', ArticleListView.as_view(), name='articles-list'),
 ]
