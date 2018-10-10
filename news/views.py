c071502f81b13eb4dda0d9a471599c736cad2705
yuriikhomych <yuriykhomich@gmail.com>
2018-10-10 21:40:13 +0300

    Create ArticleListView, FeedListView and FeedCreateView

--- news/views.py
+++ news/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .forms import FeedForm
+from .models import Feed, Article
 
-# Create your views here.
+
+
+class ArticleListView(ListView):
+    template_name = 'news/articles_list.html'
+    queryset = Article.objects.all()
+
+
+class FeedListView(ListView):
+    template_name = 'news/feeds_list.html'
+    queryset = Feed.objects.all()
+
+
+class FeedCreateView(CreateView):
+    template_name = 'news/new_feed.html'
+    form_class = FeedForm
+    queryset = Feed.objects.all()
+    success_url = '/news/feeds/'
