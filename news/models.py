244989c1c37e5c08d8b12a6cf2f2b3d4b624a7e5
yuriikhomych <yuriykhomich@gmail.com>
2018-10-10 21:37:13 +0300

    Create Feed and articles models

--- news/models.py
+++ news/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+
+class Feed(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField(unique=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    publication_date = models.DateTimeField()
+    trend = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
