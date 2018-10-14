114967f290f3b89bb43c6fc9135efa3d37a55b93
yuriikhomych <yuriykhomich@gmail.com>
2018-10-14 19:21:10 +0300

    Add celery tasks

---
+++ news/tasks.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+
+from .models import Feed
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+
+@shared_task()
+def update_all_feed_articles_task():
+    """ Updates articles for all RSS/Atom feeds """
+    feeds = Feed.objects.all()
+    for feed in feeds:
+        logger.info("Retrieving Articles for " + feed.title)
+        feed.get_feed_articles()
