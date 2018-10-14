986c026e8e6eab2e02a4d515a45095f79484145b
yuriikhomych <yuriykhomich@gmail.com>
2018-10-14 19:19:41 +0300

    Add ArticleTrendListView to urls

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import FeedCreateView, FeedListView, ArticleListView
+from .views import FeedCreateView, FeedListView, ArticleListView, ArticleTrendListView
 
 app_name = 'news'
 urlpatterns = [
     path('feeds/create', FeedCreateView.as_view(), name='feeds-create'),
     path('feeds/', FeedListView.as_view(), name='feeds-list'),
     path('articles/', ArticleListView.as_view(), name='articles-list'),
+    path('trend-articles/', ArticleTrendListView.as_view(), name='articles-trend-list'),
 ]
